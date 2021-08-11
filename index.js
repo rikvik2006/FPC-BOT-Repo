ed42c0657afa4048e4c83ccaa2a6726614032265
rikvik2006 <75540124+rikvik2006@users.noreply.github.com>
2021-08-11 12:27:02 +0200

    Primo commit del bot
    Per ora funziona tutto.
    
    Welcome,
    Help,
    Fonts

---
+++ index.js
@@ -0,0 +1,112 @@
+const Discord = require('discord.js');
+const Canvas = require('canvas');
+const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL','REACTION']});
+const canvas = Canvas.createCanvas(700, 250);
+const context = canvas.getContext('2d');
+
+client.login('ODczODg0MzU0NzE4MzU1NDY2.YQ-6Og.XBtWESrBppHw6JVtI4u5J7-qfLM');
+
+
+client.once ('ready', () => {
+    console.log ("FPC BOT E' ONLINE");
+
+    client.user.setActivity('i tuoi sentimenti', {type: 'WATCHING'});
+})
+
+
+client.on ('message', (message) => {
+    if (message.content === 'sus-help') {
+
+        const help_embed = new Discord.MessageEmbed()
+
+        .setTitle ('HELP')
+        .setColor('#CCCCCC')
+        .setDescription('Tutti i comadi')
+        .addField('sus-help', '```Tutti i comandi```')
+        .setFooter('FPC', 'https://discohook.org/static/discord-avatar.png')
+
+        message.channel.send(help_embed)
+    }
+})
+
+
+
+
+client.on('guildMemberAdd', async member => {
+	const channel = member.guild.channels.cache.find(ch => ch.name === '💎boost-welcome💎');
+	if (!channel) return;
+
+   
+    // Create a canvas and access the 2d context
+    const canvas = Canvas.createCanvas(700, 250)
+    const ctx = canvas.getContext('2d')
+
+	const { guild } = member
+
+	const { MessageAttachment } = require('discord.js')
+    // Load the background image and draw it to the canvas
+    const background = await Canvas.loadImage('./wallpaper.png');
+    const path = require('path')
+
+    Canvas.registerFont(path.join(__dirname, "fonts", "Dosis-Bold.ttf"), { family: "Dosis"});
+    
+
+
+
+	let x = 0
+    let y = 0
+    ctx.drawImage(background, x, y)
+
+
+    // Load the user's profile picture and draw it
+    const pfp = await Canvas.loadImage(
+      member.user.displayAvatarURL({
+        format: 'png',
+		format: 'webp',
+		format: 'jpg',
+		format: 'jpeg',
+		format: 'gif'
+		
+      })
+	)
+	
+
+    x = canvas.width / 2 - pfp.width / 2
+    y = 25
+
+
+    ctx.drawImage(pfp, x, y)
+    // Display user text
+    ctx.fillStyle = '#0a0a0a' // White text
+    ctx.font = '35px Dosis'    //sans-serif
+    let text = `Welcome ${member.user.tag}!`
+    x = canvas.width / 2 - ctx.measureText(text).width / 2
+    ctx.fillText(text, x, 60 + pfp.height)
+    // Display member count
+    ctx.font = '30px Dosis'
+    text = `Member #${guild.memberCount}`
+    x = canvas.width / 2 - ctx.measureText(text).width / 2
+    ctx.fillText(text, x, 100 + pfp.height)
+    // Attach the image to a message and send it
+    //const attachment = new MessageAttachment(canvas.toBuffer())
+	const attachment = new Discord.MessageAttachment(canvas.toBuffer(), 'say-goodbye.png');
+
+    channel.send(`Bella gianda, ${member}! Spero che te abbia portato i biscotti <a:hei:874591082812440586>
+	`, attachment)
+
+});
+
+
+
+client.on('message', message => {
+	if (message.content === 'sus-join') {
+		client.emit('guildMemberAdd', message.member);
+	}
+});
+
+
+
+client.on('nitroBoost', (booster) => {
+	client.channels.get('1234567890').send(`${booster} boosted the server!`)
+	client.addRole(booster.guild.roles.find(a => a.name === 'Nitro Booster'))
+ })
